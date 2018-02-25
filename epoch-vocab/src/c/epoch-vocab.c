4f30d5fa7d7a6d706ba0304a5b56bb7a4690b760
JMLN <jeanmartinezleninan@live.fr>
2018-02-25 15:56:37 +0100

    part 2 of the tutorial

--- epoch-vocab/src/c/epoch-vocab.c
+++ epoch-vocab/src/c/epoch-vocab.c
@@ -3,18 +3,34 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
-static GFont s_text_font;
+static GFont s_time_font;
+
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void main_window_load(Window *window) {
+	//Window
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
+
+	//Background
+	s_background_bitmap = gbitmap_create_with_resource(
+		RESOURCE_ID_IMAGE_BACKGROUND);
+	s_background_layer = bitmap_layer_create(bounds);	
+	bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+
+	layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+	//Time
+	s_time_font = fonts_load_custom_font(
+		resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 
 	s_time_layer = text_layer_create(
 		GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_text_color(s_time_layer, GColorBlack);
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+	text_layer_set_font(s_time_layer, s_time_font);
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
 	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -36,6 +52,8 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void main_window_unload(Window *window) {
 	text_layer_destroy(s_time_layer);
+	
+	fonts_unload_custom_font(s_time_font);
 }
 
 static void init() {
@@ -44,7 +62,7 @@ static void init() {
 		.load = main_window_load,
 		.unload = main_window_unload
 	});
-	
+	window_set_background_color(s_main_window, GColorBlack);	
 	window_stack_push(s_main_window, true);
 	
 	update_time();
