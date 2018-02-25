5cff76d137d2c993fa7533c3bee01c1dee2a4469
JMLN <jeanmartinezleninan@live.fr>
2018-02-25 14:31:35 +0100

    fix display

--- epoch-vocab/src/c/epoch-vocab.c
+++ epoch-vocab/src/c/epoch-vocab.c
@@ -10,12 +10,14 @@ static void main_window_load(Window *window) {
 	GRect bounds = layer_get_bounds(window_layer);
 
 	s_time_layer = text_layer_create(
-		GRect(0, PBL_IF_ROUND_ELSE(58, 0), bounds.size.w, 50));
+		GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
 	text_layer_set_background_color(s_time_layer, GColorClear);
 	text_layer_set_text_color(s_time_layer, GColorBlack);
-	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_DROID_SERIF_28_BOLD));
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+	layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void update_time() {
